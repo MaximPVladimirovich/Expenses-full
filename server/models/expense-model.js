c685b0b3ec9df6db051c34f9f666e121e793ead8
maxim bleeker <maxim.bleeker@gmail.com>
2021-07-05 19:11:30 -0400

    Added expenses model

---
+++ server/models/expense-model.js
@@ -0,0 +1,34 @@
+const mongoose = require(`mongoose`)
+
+const ExpenseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: 'Title is required'
+  },
+  amount: {
+    type: Number,
+    min: 0,
+    required: 'Amount is required'
+  },
+  catagory: {
+    type: String,
+    trim: true,
+    required: 'Catagory is required'
+  },
+  incurred_on: {
+    type: Date,
+    default: Date.now
+  },
+  recorded_by: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default mongoose.model('Expense', ExpenseSchema)
